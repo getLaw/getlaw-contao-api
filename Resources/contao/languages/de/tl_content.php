c92adc57c0873e9a6ef7b1c1626f0ca0f8db9742
Johannes Meibers <j.meibers@meibers-datenschutz.de>
2020-09-09 17:25:25 +0000

    Update tl_content.php

--- Resources/contao/languages/de/tl_content.php
+++ Resources/contao/languages/de/tl_content.php
@@ -25,10 +25,10 @@ $GLOBALS['TL_LANG'][$table]['renewConfirm']             = 'Wollen Sie den Text j
 /**
  * Fields
  */
-$GLOBALS['TL_LANG'][$table]['getlawtextkey']            = ['getLaw-Textkey', 'Bitte geben Sie den Textkey ein, den Sie in der Liste mit Ihren Texten auf der Seite https://getLaw.de/ erstellt haben.'];
-$GLOBALS['TL_LANG'][$table]['getlawdisableautorenew']   = ['Automaitschen Abruf deaktiveren', 'Ist der Haken gesetzt, wird der Text nicht automatisch erneuert.'];
+$GLOBALS['TL_LANG'][$table]['getlawtextkey']            = ['API-Schlüssel', 'Bitte geben Sie den individuellen API-Schlüssel Ihres Textes ein. Diesen erhalten Sie auf www.getLaw.de.'];
+$GLOBALS['TL_LANG'][$table]['getlawdisableautorenew']   = ['Automatischen Abruf deaktivieren', 'Falls gewünscht, können Sie den automatischen Abruf durch Setzen eines Häkchens deaktivieren.'];
 $GLOBALS['TL_LANG'][$table]['savedon']                  = ['Speicherzeit', 'Zeitpunkt an dem der Text das letzte Mal abgerufen und gespeichert wurde.'];
-$GLOBALS['TL_LANG'][$table]['getlawdata']               = ['Daten der Schnittstelle', 'Daten die über die Schnittstelle geladen wurde.'];
+$GLOBALS['TL_LANG'][$table]['getlawdata']               = ['Daten der Schnittstelle', 'Daten die über die Schnittstelle geladen wurden.'];
 
 
 /**
